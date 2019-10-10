a090bc8c228a346bd1a762df8f71e5d91768c80f
dan-kwiat <dan.kwiat@gmail.com>
2019-10-10 14:58:32 +0100

    feat(): replace renderForm with renderSearch

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@
 // exports.__esModule = true;
 import React from 'react'
 import ReactDOM from 'react-dom'
-import CharityBaseForm from 'charity-base-form'
-import 'charity-base-form/es/index.css'
+// import CharityBaseForm from 'charity-base-form'
+import CharityBaseSearch from 'charity-base-search'
+import 'charity-base-search/es/index.css'
+// import 'charity-base-form/es/index.css'
 // import 'charity-base-form/es/index.scss'
 
 function CharityBase(apiKey, options) {
   return {
-    renderForm: function(targetNode, props, callback) {
+    // renderForm: function(targetNode, props, callback) {
+    //   var allProps = props || {}
+    //   allProps.apiKey = apiKey
+    //   var reactElement = React.createElement(CharityBaseForm, allProps, null)
+    //   ReactDOM.render(reactElement, targetNode, callback)
+    // },
+    renderSearch: function(targetNode, props, callback) {
       var allProps = props || {}
       allProps.apiKey = apiKey
-      var reactElement = React.createElement(CharityBaseForm, allProps, null)
+      var reactElement = React.createElement(CharityBaseSearch, allProps, null)
       ReactDOM.render(reactElement, targetNode, callback)
-    }
+    },
   }
 }
 
