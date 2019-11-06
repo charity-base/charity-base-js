7d0d09a03875fcdce2c1ff0d0cbaa56646a7114d
dan-kwiat <dan.kwiat@gmail.com>
2019-11-06 15:39:07 +0000

    feat(): put render method on element instance

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 "use strict";
-// exports.__esModule = true;
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import CharityBaseForm from 'charity-base-form'
-import CharityBaseSearch from 'charity-base-search'
-import 'charity-base-search/es/index.css'
-// import 'charity-base-form/es/index.css'
-// import 'charity-base-form/es/index.scss'
+import CharityBaseForm from 'charity-base-form'
+import 'charity-base-form/es/index.css'
+
+function targetNode(elementId) {
+  var target = document.getElementById(elementId)
+  if (!target) {
+    throw "CharityBase render couldn't find an element with id '" + elementId + "'"
+  }
+  return target
+}
+
+function cbElement(reactElement) {
+  return {
+    component: reactElement,
+    // rename render -> mount like stripe?
+    render: function(elementId, callback) {
+      ReactDOM.render(reactElement, targetNode(elementId), callback)
+    }
+  }
+}
 
 function CharityBase(apiKey, options) {
   return {
-    // renderForm: function(targetNode, props, callback) {
-    //   var allProps = props || {}
-    //   allProps.apiKey = apiKey
-    //   var reactElement = React.createElement(CharityBaseForm, allProps, null)
-    //   ReactDOM.render(reactElement, targetNode, callback)
-    // },
-    renderSearch: function(targetNode, props, callback) {
+    createAutofill: function(props) {
+      // re-implement using spread so as not to mutate props?
       var allProps = props || {}
       allProps.apiKey = apiKey
-      var reactElement = React.createElement(CharityBaseSearch, allProps, null)
-      ReactDOM.render(reactElement, targetNode, callback)
-    },
+      var reactElement = React.createElement(CharityBaseForm, allProps, null)
+      return cbElement(reactElement)
+    }
   }
 }
 
--- webpack.conf.js
+++ webpack.conf.js
@@ -1,128 +1,75 @@
-// const Uglify = require("uglifyjs-webpack-plugin");
-// const proxyTarget = 'https://whitelabeling.gooddata.com/';
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  plugins: [
-
-  ],
+  entry: './src',
+  output: {
+    filename: 'index.js',
+    libraryTarget: 'window'
+  },
+  resolve: {
+    alias: {
+      'react': 'preact/compat',
+      'react-dom': 'preact/compat',
+      // Not necessary unless you consume a module using `createClass`
+      'create-react-class': 'preact/compat/lib/create-react-class',
+      // Not necessary unless you consume a module requiring `react-dom-factories`
+      'react-dom-factories': 'preact/compat/lib/react-dom-factories'
+    }
+  },
   module: {
     rules: [
-
-    ],
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+        ],
+      }
+    ]
   },
-};
-
-module.exports = {
-    entry: './src',
-    output: {
-        // path: './dist',
-        filename: 'index.js',
-        // library: 'CharityBase',
-        libraryTarget: 'window'
-    },
-    resolve: {
-        alias: {
-            'react': 'preact/compat',
-            'react-dom': 'preact/compat',
-            // Not necessary unless you consume a module using `createClass`
-            'create-react-class': 'preact/compat/lib/create-react-class',
-            // Not necessary unless you consume a module requiring `react-dom-factories`
-            'react-dom-factories': 'preact/compat/lib/react-dom-factories'
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // fallback to style-loader in development
-                    process.env.NODE_ENV !== 'production'
-                        ? 'style-loader'
-                        : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                      loader: MiniCssExtractPlugin.loader,
-                      options: {
-                        hmr: process.env.NODE_ENV === 'development',
-                      },
-                    },
-                    'css-loader',
-                ],
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-          // Options similar to the same options in webpackOptions.output
-          // all options are optional
-          filename: 'index.css',
-          // filename: '[name].css',
-          // chunkFilename: '[id].css',
-          ignoreOrder: false, // Enable to remove warnings about conflicting order
-        }),
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // all options are optional
+      filename: 'index.css',
+      // filename: '[name].css',
+      // chunkFilename: '[id].css',
+      ignoreOrder: false, // Enable to remove warnings about conflicting order
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
     ],
-    optimization: {
-      minimizer: [
-          new UglifyJsPlugin({
-            uglifyOptions: {
-              output: {
-                comments: false,
-              },
-            },
-          }),
-      ],
-      // splitChunks: {
-      //   chunks: 'all',
-      // },
-    },
-    devServer: {
-        contentBase: './demo',
-        port: 3000,
-        // https: true,
-        // proxy: {
-        //     '/gdc': {
-        //         cookieDomainRewrite: "localhost",
-        //         target: proxyTarget,
-        //         secure: false,
-        //         changeOrigin: true,
-        //         onProxyReq: proxyReq => {
-        //             // Browers may send Origin headers even with same-origin
-        //             // requests. To prevent CORS issues, we have to change
-        //             // the Origin to match the target URL.
-        //             if (proxyReq.getHeader('origin')) {
-        //                 proxyReq.setHeader('origin', proxyTarget);
-        //             }
-        //         }
-        //     },
-        //     "/packages": {
-        //         "changeOrigin": true,
-        //         "cookieDomainRewrite": "localhost",
-        //         "target": proxyTarget
-        //     },
-        //     "/lib": {
-        //         "changeOrigin": true,
-        //         "cookieDomainRewrite": "localhost",
-        //         "target": proxyTarget
-        //     },
-        //     "/images": {
-        //         "changeOrigin": true,
-        //         "cookieDomainRewrite": "localhost",
-        //         "target": proxyTarget
-        //     },
-        //     "/*.html": {
-        //         "cookieDomainRewrite": "localhost",
-        //         "changeOrigin": true,
-        //         "secure": false,
-        //         "target": proxyTarget
-        //     }
-        // }
-    }
+    // splitChunks: {
+    //   chunks: 'all',
+    // },
+  },
+  devServer: {
+    contentBase: './demo',
+    port: 3000,
+  }
 }
